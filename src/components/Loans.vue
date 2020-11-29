b067c40e9aac1c3c047322e70a2c5b73bba272f6
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-11-28 22:02:40 -0500

    user info display on /Loans

--- src/components/Loans.vue
+++ src/components/Loans.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="max-w-md m-auto py-10">
     <div class="text-red" v-if="error">{{ error }}</div>
-    <ul class="list-reset mt-4">
-      <li class="py-3" v-for="loan in loans" :key="loan.id" :loan="loan">
-        <div class="flex items-center justify-between flex-wrap">
-          <p class="block flex font-mono font-semibold items-center">
-            {{ loan.borrower }} &mdash; {{ loan.coBorrower }}
-          </p>
-          <p class="block font-mono font-semibold">{{ loan.vehicleMake }}</p>
-        </div>
-      </li>
-    </ul>
+    <div class="grid grid-rows-3 grid-cols-3 h-20 border-solid border-2 border-gray-400 rounded-2xl p-4" v-for="loan in loans" :key="loan.id" :loan="loan">
+      <div class="row-start-1 column-start-1 text-left">Welcome:</div>
+      <div class="row-start-2 column-start-1 text-left">Address:</div>
+      <div class="row-start-3 column-start-1 text-left">Email:</div>
+      <div class="row-start-1 row-end-2 col-start-2 col-end-4 text-left">{{loan.borrower}}</div>
+      <div class="row-start-2 col-start-2 col-end-4 text-left">{{loan.borrowerAddress}}</div>
+      <div class="row-start-3 col-start-2 col-end-4 text-left uppercase">{{loan.borrowerEmail}}</div>
+    </div>
+    <div class="grid">
+      <p>placeholder</p>
+    </div>
   </div>
 </template>
 
